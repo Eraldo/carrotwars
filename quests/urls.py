58628499555ea95c0c6123376eada026dfd39014
Eraldo Helal <eraldo@eraldo.org>
2012-11-25 17:08:44 +0100

    cleanup

---
+++ quests/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, url
+from django.views.generic import DetailView, ListView
+from quests.models import Quest
+from quests.views import QuestListView
+
+urlpatterns = patterns('',
+    # ex: /quests/
+    url(r'^$',
+        QuestListView.as_view(),
+        name='list'),
+    # ex: /quests/4/
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Quest,
+            template_name='quests/detail.html'),
+        name='detail'),
+)
