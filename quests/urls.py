59d93e6978d249c2c7f471881b5a5fbd74f3076b
Eraldo Helal <eraldo@eraldo.org>
2012-11-28 19:35:23 +0100

    added update view for quests and minor edits

--- quests/urls.py
+++ quests/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, FormView
 from quests.models import Quest
 from quests.views import QuestListView
 
@@ -10,8 +10,12 @@ urlpatterns = patterns('',
         name='list'),
     # ex: /quests/4/
     url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
+        # DetailView.as_view(
+        #     model=Quest,
+        #     template_name='quests/detail.html'),
+        # name='detail'),
+        UpdateView.as_view(
             model=Quest,
-            template_name='quests/detail.html'),
+            ),
         name='detail'),
 )
--- quests/views.py
+++ quests/views.py
@@ -11,3 +11,14 @@ class QuestListView(ListView):
         context['owned_quests'] = Quest.quests.get_owned_quests(self.request.user)
         context['assigned_quests'] = Quest.quests.get_assigned_quests(self.request.user)
         return context
+
+class QuestListsView(ListView):
+    context_object_name = 'all_quests'
+    template_name = 'quests/list.html'
+
+    model = Quest
+    def get_context_data(self, **kwargs):
+        context = super(QuestListView, self).get_context_data(**kwargs)
+        context['owned_quests'] = Quest.quests.get_owned_quests(self.request.user)
+        context['assigned_quests'] = Quest.quests.get_assigned_quests(self.request.user)
+        return context
