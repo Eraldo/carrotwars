1f1b4fdf1dd3d745419c61c96981f743659d617d
Eraldo Helal <eraldo@eraldo.org>
2012-11-30 18:48:35 +0100

    refactored relations

--- relations/models.py
+++ relations/models.py
@@ -2,12 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class RelationManager(models.Manager):
-    def get_owned_relations(self, user):
+    def owned(self, user):
         return super(RelationManager, self).get_query_set().filter(owner=user)
-    def get_assigned_relations(self, user):
+    def assigned(self, user):
         return super(RelationManager, self).get_query_set().filter(quester=user)
-    def get_all_relations(self):
-        return super(RelationManager, self).get_query_set()
 
 class Relation(models.Model):
     owner = models.ForeignKey(User, related_name='relation_owner')
@@ -19,11 +17,10 @@ class Relation(models.Model):
         ('A', 'accepted'),
     )
     status = models.CharField(default='C', max_length=1, choices=STATUS)
-    relations = RelationManager()
+    objects = RelationManager()
     
     def __unicode__(self):
         return u'%s - %s' % (self.owner, self.quester)
     
     class Meta:
-        #verbose_name_plural = "relations"
         unique_together = ("owner", "quester")
